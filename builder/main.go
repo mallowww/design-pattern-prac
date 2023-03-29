8ce63df653020793ee5164a40c77d728b18b61e2
mell <chanon4817@gmail.com>
2023-03-29 12:42:03 +0700

    feat: client main

--- builder/main.go
+++ builder/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+	normalBuilder := getBuilder("normal")
+	iglooBuilder := getBuilder("igloo")
+
+	director := newDirector(normalBuilder)
+	normalHouse := director.buiildHouse()
+
+	fmt.Printf("Normal House Window type: %s\n", normalHouse.windowType)
+	fmt.Printf("Normal House Door type: %s\n", normalHouse.doorType)
+	fmt.Printf("Normal House Num floor: %d\n", normalHouse.floor)
+
+	director.setBuilder(iglooBuilder)
+	iglooHouse := director.buiildHouse()
+
+	fmt.Printf("\nIgloo House Window type: %s\n", iglooHouse.windowType)
+	fmt.Printf("Igloo House Door type: %s\n", iglooHouse.doorType)
+	fmt.Printf("Igloo House Num floor: %d\n", iglooHouse.floor)
 
 }
